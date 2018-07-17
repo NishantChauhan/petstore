8090d43013e18f2145c1f1fd8d18102f23209b2e
Nishant Chauhan <CHAUHANN@amdocs.com>
2018-07-17 08:19:37 -0400

    Initial Commit

---
+++ src/main/java/com/petstore/PetStoreAppApplication.java
@@ -0,0 +1,12 @@
+package com.petstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetStoreAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetStoreAppApplication.class, args);
+	}
+}
